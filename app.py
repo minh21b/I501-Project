487cb139fc73d7b4c14fed11f927571333d12462
minh21b <45769838+minh21b@users.noreply.github.com>
2024-04-18 13:05:04 -0400

    update env.yml

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import os
 import pickle
 from utils.b2 import B2
-from utils.modeling import *
+#from utils.modeling import *
 import streamlit as st
 from dotenv import load_dotenv
-import numpy as np
-from implicit.als import AlternatingLeastSquares
-from sklearn.metrics import mean_squared_error
-from scipy.sparse import coo_matrix
-import category_encoders as ce
-import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, balanced_accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 # import warnings
 # warnings.filterwarnings('ignore', category=SettingWithCopyWarning)
